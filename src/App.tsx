fa5aef23fae113395466ca0ec726e29911423189
Rodolfo Fernando Juarez <rfjuarez@gmail.com>
2020-09-12 18:17:34 -0300

    Initial concept test for weather service

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React,{useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import { IpApiResponse } from './model/ipapi-response';
-import {IpApiClient} from "./services/ipapi-service";
+import {AppServicesMock} from "./services/app-services";
+import {WeatherForecastResponse} from "./model/weather-response"
 
-const  fetchUserLocation = async() =>{
-  const ipApiClient = new IpApiClient();
-  return await ipApiClient.getCurrentLocation();
+const fetchUserLocation = async () => {
+    const appService = new AppServicesMock();
+    return await appService.fetchWeatherForecastByCurrentLocation();
 };
 
 function App() {
-  const [userLocation, setUserLocation] = useState<IpApiResponse|undefined>(undefined);
-  useEffect(()=>{
-    fetchUserLocation().then((location)=>setUserLocation(location));
-  },[userLocation]);
-  return (
-    <div>      
-      {JSON.stringify(userLocation)}
-    </div>
-  );
+    const [userLocation, setUserLocation] = useState<WeatherForecastResponse | undefined>(undefined);
+    useEffect(() => {
+        fetchUserLocation().then((location) => setUserLocation(location));
+    },[] );
+    return (
+        <div>
+            {JSON.stringify(userLocation)}
+        </div>
+    );
 }
 
 export default App;
