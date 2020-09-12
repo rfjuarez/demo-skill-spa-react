0df09f4d6701701714be8a4d26834ba4d553720e
Rodolfo Fernando Juarez <rfjuarez@gmail.com>
2020-09-12 03:05:57 -0300

    Added ipapi client

---
+++ src/services/ipapi-service.ts
@@ -0,0 +1,10 @@
+import {HttpClient} from './http-client';
+import { IpApiResponse } from '../model/ipapi-response';
+
+export class IpApiClient extends HttpClient {
+  public constructor() {
+    super("http://ip-api.com");
+  };
+
+  public getCurrentLocation = async() => this.instance.get<IpApiResponse>("/json");
+};
