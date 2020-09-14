0ee480a7e61588830353202623c2a656bfa9f745
Rodolfo Fernando Juarez <rfjuarez@gmail.com>
2020-09-14 04:15:20 -0300

    Update App.ts with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import WeatherMain from "./components/WeatherMain/WeatherMain";
+import {Provider} from 'react-redux'
+import {storeInstance} from "./store/store";
 
 function App() {
-    return (<WeatherMain/>);
+    return (
+        <Provider store={storeInstance}>
+            <WeatherMain/>
+        </Provider>);
 }
 
 export default App;
--- src/components/WeatherMain/WeatherMain.tsx
+++ src/components/WeatherMain/WeatherMain.tsx
@@ -1,6 +1,4 @@
-import {AppServicesMock, AppService} from "../../services/app-services";
-import React, {useEffect, useState} from "react";
-import {WeatherResponse} from "../../model/weather-response";
+import React, {Dispatch, useEffect, useState} from "react";
 import Container from "../Layout/Container";
 import CurrentContainer from "../Layout/CurrentContainer";
 import WeatherBackgroundImage from "../Layout/ContentWithImage";
@@ -13,68 +11,77 @@ import Sidebar from "../Sidebar/Sidebar";
 import SidebarItem from "../Sidebar/SidebarItem";
 import {City} from "../../model/app";
 import Location from "../Location/Location";
+import {connect, ConnectedProps} from "react-redux";
+import {AppServiceActionMessage} from "../../store/reducers/app-state/app-service-actions";
+import {loadWeatherByCurrentLocation} from "../../store/actions"
+import {ApplicationState} from "../../store/root-reducer";
+import {AppServicesMock} from "../../services/app-services";
 
-const fetchUserLocation = async () => {
-    const appService = new AppServicesMock();
-    return await appService.fetchWeatherForecastByCurrentLocation();
-};
-const fetchLocations = async ():Promise<City[]> =>{
+const fetchLocations = async (): Promise<City[]> => {
     const appService = new AppServicesMock();
     return await appService.getCities();
 }
-const WeatherMain =() =>{
-    const [userLocation, setUserLocation] = useState<WeatherResponse | undefined>(undefined);
-    const [showSidebar, setShowSidebar] = useState<boolean>(false);
-    const [cities,setCities]=useState<City[] | undefined>(undefined);
-    useEffect(() => {
-        fetchUserLocation().then((location) => setUserLocation(location));
-        fetchLocations().then(cities=>setCities(cities));
-    },[] );
+const mapActionsToProps = (dispatch: Dispatch<AppServiceActionMessage>) => ({
+    loadWeatherByCurrentPosition: async () => loadWeatherByCurrentLocation()(dispatch)
+});
 
-    const changeLocation=(event: React.MouseEvent<HTMLDivElement, MouseEvent>)=> {
+const mapStateToProps = (state: ApplicationState) => ({
+    appState: state.appService
+});
+
+const connector = connect(mapStateToProps, mapActionsToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+
+const WeatherMain = (props: PropsFromRedux) => {
+    const [showSidebar, setShowSidebar] = useState<boolean>(false);
+    const [cities, setCities] = useState<City[] | undefined>(undefined);
+    useEffect(() => {
+        props.loadWeatherByCurrentPosition();
+        fetchLocations().then(cities => setCities(cities));
+    }, []);
+
+    const changeLocation = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         event.preventDefault();
-        const _s=!showSidebar
+        const _s = !showSidebar
         setShowSidebar(_s);
     };
-    const buildSidebarItems = ():React.ReactNode =>{
-        const citiesOptions= cities?.map((c,index)=>{
-            return <SidebarItem key={`SidebarItem-${index}`}><Location  city={c}/></SidebarItem>
+    const buildSidebarItems = (): React.ReactNode => {
+        const citiesOptions = cities?.map((c, index) => {
+            return <SidebarItem key={`SidebarItem-${index}`}><Location city={c}/></SidebarItem>
         });
-        if(citiesOptions){
+        if (citiesOptions) {
             return <Sidebar show={showSidebar}>{citiesOptions}</Sidebar>
-        }else{
+        } else {
             return null;
         }
-
     }
 
     return (
         <>
             {buildSidebarItems()}
-
-
-        <Container>
-            <CurrentContainer>
-                <WeatherBackgroundImage iconCode={"02d"}>
-                    <WeatherCurrentLocation dt={"22/22/444"}
-                                            location={"Cordoba - Arg"}
-                                            weekDay={"Lunes"}
-                                            action={changeLocation}
-                    />
-                    <WeatherCurrentMeasure iconCode={"02d"} temp={25} description={"Soleado"}/>
-                </WeatherBackgroundImage>
-            </CurrentContainer>
-            <ForecastContainer>
-                <WeatherWeek>
-                    <WeatherDay day={"Lunes"} temp={23} tempMin={12} tempMax={24} units={"c"} iconCode={"02d"}/>
-                    <WeatherDay day={"Martes"} temp={23} tempMin={11} tempMax={25} units={"c"} iconCode={"02d"}/>
-                    <WeatherDay day={"Miercoles"} temp={23} tempMin={13} tempMax={25} units={"c"} iconCode={"01d"}/>
-                    <WeatherDay day={"Jueves"} temp={25} tempMin={16} tempMax={27} units={"c"} iconCode={"01d"}/>
-                    <WeatherDay day={"Viernes"} temp={26} tempMin={18} tempMax={27} units={"c"} iconCode={"01d"}/>
-                </WeatherWeek>
-            </ForecastContainer>
-        </Container>
-            </>
+            <Container>
+                <CurrentContainer>
+                    <WeatherBackgroundImage iconCode={"02d"}>
+                        <WeatherCurrentLocation dt={"22/22/444"}
+                                                location={"Cordoba - Arg"}
+                                                weekDay={"Lunes"}
+                                                action={changeLocation}
+                        />
+                        <WeatherCurrentMeasure iconCode={"02d"} temp={25} description={"Soleado"}/>
+                    </WeatherBackgroundImage>
+                </CurrentContainer>
+                <ForecastContainer>
+                    <WeatherWeek>
+                        <WeatherDay day={"Lunes"} temp={23} tempMin={12} tempMax={24} units={"c"} iconCode={"02d"}/>
+                        <WeatherDay day={"Martes"} temp={23} tempMin={11} tempMax={25} units={"c"} iconCode={"02d"}/>
+                        <WeatherDay day={"Miercoles"} temp={23} tempMin={13} tempMax={25} units={"c"} iconCode={"01d"}/>
+                        <WeatherDay day={"Jueves"} temp={25} tempMin={16} tempMax={27} units={"c"} iconCode={"01d"}/>
+                        <WeatherDay day={"Viernes"} temp={26} tempMin={18} tempMax={27} units={"c"} iconCode={"01d"}/>
+                    </WeatherWeek>
+                </ForecastContainer>
+            </Container>
+        </>
     );
 };
-export default WeatherMain;
+export default connector(WeatherMain);
