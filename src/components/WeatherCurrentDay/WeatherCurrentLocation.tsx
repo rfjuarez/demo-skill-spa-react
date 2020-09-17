7a6399dbbd20939bda0cbaf81f621a388ce9de39
Rodolfo Fernando Juarez <rfjuarez@gmail.com>
2020-09-17 01:48:30 -0300

    Replace short by large day name

--- src/components/WeatherCurrentDay/WeatherCurrentLocation.tsx
+++ src/components/WeatherCurrentDay/WeatherCurrentLocation.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./WeatherCurrentLocation.module.css"
 import pickerLocation from "../../assets/icon/picker-location.svg"
-import {epocToShortDay} from "../../utils/epoc";
+import {epocToLargeDay} from "../../utils/epoc";
 
 interface Props {
     dt: number;
@@ -12,7 +12,7 @@ interface Props {
 const WeatherCurrentLocation = (props: Props) => {
     return (
         <div className={styles["weather-current-location"]} onClick={(event) => props.action(event)}>
-            <h2 className={styles["weather-current-location--weekday"]}>{epocToShortDay(props.dt)}</h2>
+            <h2 className={styles["weather-current-location--weekday"]}>{epocToLargeDay(props.dt)}</h2>
             <img src={pickerLocation} className={styles["weather-current--picker"]} alt={"Ubicación Actual"}/>
             <span className={styles["weather-current-location--location"]}>{props.location}</span>
         </div>
